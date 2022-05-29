75fc9f5846c17ade0496618ac9b8c9d1a77cdea9
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-05-29 23:06:47 +0700

    Test Loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import BaseLayout from '@/components/layouts/BaseLayout'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeComplete", NProgress.done);
+Router.events.on("routeChangeError", NProgress.done);
+
 function MyApp({ Component, pageProps }: AppProps) {
-   Router.events.on("routeChangeStart", NProgress.start);
-   Router.events.on("routeChangeComplete", NProgress.done);
-   Router.events.on("routeChangeError", NProgress.done);
    return (
       <>
          <BaseLayout>
