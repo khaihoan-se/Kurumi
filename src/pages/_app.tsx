67aeb7f5b7439f1f4bdff49b6b15f6ec0f1bcf46
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-05-29 22:51:14 +0700

    Test Loading NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import BaseLayout from '@/components/layouts/BaseLayout'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 function MyApp({ Component, pageProps }: AppProps) {
+   Router.events.on('routeChangeStart', () => {
+      NProgress.start();
+   })
+
+   Router.events.on('routeChangeComplete', () => {
+      NProgress.done();
+   })
    return (
       <>
          <BaseLayout>
